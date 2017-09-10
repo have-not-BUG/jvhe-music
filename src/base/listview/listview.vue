6c3bc2304067e185eb4030887d4cbe994abb839e
have-not-BUG <lxyxdylglc@qq.com>
2017-09-10 17:54:03 +0800

    最终完善歌手数据，完成歌手页面雏形

---
+++ src/base/listview/listview.vue
@@ -0,0 +1,54 @@
+<template>
+  <scroll class="list-view" :data="data">
+    <ul class="singers-wrapper">
+      <li v-for="includeClassifiedData in data">
+        <h2>  {{includeClassifiedData.title}}</h2>
+        <ul>
+          <li v-for=" item in includeClassifiedData.items">
+            <img v-lazy="item.avatar" :alt="item.name">
+            <span>{{item.name}}</span>
+          </li>
+        </ul>
+
+      </li>
+    </ul>
+
+  </scroll>
+
+
+</template>
+
+<script>
+  import Scroll from 'base/scroll/scroll'
+
+  export default {
+    props: {
+      data: {
+        type: Array,
+        default: []
+      }
+    },
+    components: {
+      Scroll
+    }
+
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  .list-view{
+    height 100%
+    overflow hidden
+    .singers-wrapper{
+      position: relative
+      width: 100%
+      overflow: hidden
+    }
+  }
+
+
+
+</style>
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,30 +1,31 @@
 <template>
-  <div class="hello">
-    <h1 @click=" _getQQSingerList">{{ msg }}</h1>
+  <div class="singer">
+  <listview :data="optimizedSingerList"></listview>
   </div>
 </template>
 
 <script>
   import { getQQSingerList }  from  'api/qqSingerList'
   import { ERROR_OK } from 'api/config'
+  import Singer from 'common/js/singer'
+  import Listview from 'base/listview/listview'
 
-  const HOT_SINGER_NAME = '热门'
+  const SINGER_TYPE = '热门'
   const HOT_SINGER_LENGTH = 10
 
   export default {
     name: 'singer',
     data () {
       return {
-        msg: '歌手页面',
-        singers: []
+        optimizedSingerList: []
       }
     },
     methods: {
       _getQQSingerList() {
         getQQSingerList().then(res => {
           if (res.code === ERROR_OK) {
-            this.singers = res.data.list
-            this.optimizeQQSingerList(this.singers)
+            this.optimizedSingerList = this.optimizeQQSingerList(res.data.list)
+            console.log('this.optimizedSingerList', this.optimizedSingerList)
           }
         }).catch(err => {
           alert('获取QQ歌手数据出错了,请刷新重试或者联系本人', err)
@@ -33,18 +34,18 @@
       optimizeQQSingerList(list){
         let singersData = {
           hot: {
-            title: HOT_SINGER_NAME,
+            title: SINGER_TYPE,
             items: []
           }
         };
         list.forEach((item, index) => {
           if (index < HOT_SINGER_LENGTH) {
             singersData.hot.items.push(
-              {
-                name: item.Fsinger_name,
-                mid: item.Fsinger_mid,
-                avatar: `https://y.gtimg.cn/music/photo_new/T001R150x150M000${item.Fsinger_mid}.jpg?max_age=2592000`
-              }
+              new Singer(
+                {
+                  name: item.Fsinger_name,
+                  mid: item.Fsinger_mid,
+                })
             )
 
           }
@@ -57,21 +58,45 @@
             }
 
           }
-          singersData[initials].items.push({
-              name: item.Fsinger_name,
-              mid: item.Fsinger_mid,
-              avatar: `https://y.gtimg.cn/music/photo_new/T001R150x150M000${item.Fsinger_mid}.jpg?max_age=2592000`
-            }
+          singersData[initials].items.push(
+            new Singer(
+              {
+                name: item.Fsinger_name,
+                mid: item.Fsinger_mid,
+              })
           )
 
         })
-        console.log(singersData)
+
+        let hotSingerData = [],
+          initialsSingerData = [];
+        for (let key in singersData) {
+          let val = singersData[key];
+
+          if (val.title === SINGER_TYPE) {
+            hotSingerData.push(val)
+
+          } else if (val.title.match(/[a-zA-Z]/)) {
+            initialsSingerData.push(val)
+
+          }
+
+        }
+
+        initialsSingerData.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+
+        return hotSingerData.concat(initialsSingerData)
 
       }
 
     },
     created: function () {
       this._getQQSingerList()
+    },
+    components: {
+      Listview
     }
   }
 </script>
@@ -79,5 +104,12 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
+
+  .singer{
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0
+  }
 
 </style>
