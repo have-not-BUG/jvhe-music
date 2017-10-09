02af35384f625e8417879a98c150c5e08b065921
have-not-BUG <lxyxdylglc@qq.com>
2017-10-09 08:44:32 +0800

    完成各排行榜数据的应用及基本样式处理

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="rank">
+    <ul class="rank-ul">
+      <li v-for="item in topList " v-if="topList.length" class="ranklist-wrap">
+        <div class="rank-icon">
+          <img :src="item.picUrl" :alt="item.topTitle">
+        </div>
+        <div class="rank-detail">
+          <p v-for="(detail,index) in item.songList ">
+            {{index+1}}.{{detail.songname}}-{{detail.singername}}
+          </p>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -9,7 +20,7 @@
   export default {
     data () {
       return {
-        msg: '排行页面'
+        topList: []
       }
     },
     created(){
@@ -18,7 +29,7 @@
     methods: {
       _getQQAllRankData(){
         getQQAllRankData().then(res => {
-          console.log(res.data)
+          this.topList = res.data.topList
         }).catch(err => {
           console.log('获取QQ音乐所有排行榜数据出错', err)
         })
@@ -30,5 +41,40 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+  .rank {
+    .rank-ul {
+      width 100%
+      .ranklist-wrap {
+        position relative
+        margin 15px 20px
+        background-color $color-highlight-background
+        no-wrap()
+        display: flex
+        align-items  center
+        .rank-icon {
+          height 100px
+          width 100px
+          img {
+            width 100%
+            height 100%
+          }
+        }
+        .rank-detail {
+          width calc( 100% - 120px )
+          margin-left 10px
+          p{
+            no-wrap()
+            color $color-text-d
+            font-size $font-size-small
+            margin  5px 0
+          }
+        }
+
+      }
+
+    }
+
+  }
 
 </style>
