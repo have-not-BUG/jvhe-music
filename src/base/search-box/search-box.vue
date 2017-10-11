d4ab77ec62633ae83b47f71c2541cf9609ecf723
have-not-BUG <lxyxdylglc@qq.com>
2017-10-11 15:02:50 +0800

    完成search-box部分样式

---
+++ src/base/search-box/search-box.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="search-box">
+    <i class="icon-search"></i>
+    <input class="search-input" type="search" :placeholder="placeholder">
+  </div>
+
+</template>
+
+<script>
+  export default {
+    props: {
+      placeholder: {
+        type: String,
+        default: '搜索歌曲、歌手'
+      }
+    }
+
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  .search-box {
+    display flex
+    align-items center
+    justify-content center
+    box-sizing border-box
+    border-radius 6px
+    height 40px
+    padding 10px 5px
+    background-color $color-highlight-background
+
+
+    .icon-search {
+      font-size 24px
+
+
+    }
+    .search-input {
+      flex 1
+      background-color $color-highlight-background
+      margin 0 5px
+      line-height 18px
+      font-size $font-size-medium
+      color $color-text
+    }
+  }
+
+
+</style>
--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,22 +1,33 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="search-wrap">
+    <div class="search-box-wrap">
+      <search-box></search-box>
+    </div>
+
   </div>
 </template>
 
 <script>
-export default {
-  name: 'search',
-  data () {
-    return {
-      msg: '搜索页面'
+  import searchBox from 'base/search-box/search-box'
+  export default {
+    data() {
+      return {
+        msg: '搜索页面'
+      }
+    },
+    components: {
+      searchBox
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
+  .search-wrap{
+    .search-box-wrap{
+      margin 20px
+    }
+  }
 
 </style>
