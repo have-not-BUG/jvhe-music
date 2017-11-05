70b0baf3a1bfdba329c877a739b725350debf755
have-not-BUG <lxyxdylglc@qq.com>
2017-11-05 16:41:26 +0800

    完成用户中心 基本样式

--- src/components/user-center/user-center.vue
+++ src/components/user-center/user-center.vue
@@ -1,14 +1,43 @@
 <template>
   <transition name="slide">
     <div class="user-center">
-      用户中心
+      <div class="back-parts">
+        <i class="icon-back" @click="goBack"></i>
+        <switches :switches="switches" :currentIndex="currentIndex" @select="selectIndex"></switches>
+      </div>
+      <div class="random-play-all-wrap">
+        <div class="random-play-all-button"><i class="icon-play"></i> 随机播放全部</div>
+      </div>
     </div>
   </transition>
 </template>
 
 <script>
+  import Switches from 'base/switches/switches'
 
-  export default {}
+  export default {
+    data () {
+      return {
+        currentIndex: 0,
+        switches: [
+          {name: '我喜欢的'},
+          {name: '我最近听的'}
+        ]
+
+      }
+    },
+    components: {
+      Switches
+    },
+    methods: {
+      selectIndex (index) {
+        this.currentIndex = index
+      },
+      goBack () {
+        this.$router.back()
+      }
+    }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -22,6 +51,31 @@
     right 0
     bottom 0
     background: $color-background
+    padding 10px 20px
+    .back-parts {
+      display flex
+      align-items center
+      i {
+        font-size $font-size-large-x
+        color $color-theme
+      }
+    }
+    .random-play-all-wrap {
+      position absolute
+      left 50%
+      top 72px
+      transform translate(-50%, 0)
+      text-align center
+      color $color-text-l
+      .random-play-all-button {
+        box-sizing border-box
+        width 135px
+        padding 7px 0
+        border 1px solid $color-text-l
+        border-radius 100px
+        font-size $font-size-medium
+      }
+    }
   }
 
   .slide-enter-active, .slide-leave-active {
