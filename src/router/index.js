9cb8513be9252f8e1b602c6ea4feca65bbc6a4bd
have-not-BUG <lxyxdylglc@qq.com>
2017-11-07 15:41:42 +0800

    实现按需加载相应的js文件 优化性能

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,62 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Rank from 'components/rank/rank'
-import Recommend from 'components/recommend/recommend'
-import Search from 'components/search/search'
-import Singer from 'components/singer/singer'
-import SingerDetail from 'components/singer-detail/singer-detail'
-import NotFind from 'components/notfind/notfind'
-import RecommendList from 'components/recommend-list/recommend-list'
-import RankDetail from 'components/rank-detail/rank-detail'
-import userCenter from 'components/user-center/user-center'
+
+// import Recommend from 'components/recommend/recommend'
+// import Search from 'components/search/search'
+// import Singer from 'components/singer/singer'
+// import SingerDetail from 'components/singer-detail/singer-detail'
+// import NotFind from 'components/notfind/notfind'
+// import RecommendList from 'components/recommend-list/recommend-list'
+// import RankDetail from 'components/rank-detail/rank-detail'
+// import userCenter from 'components/user-center/user-center'
+// import Rank from 'components/rank/rank'
 
 Vue.use(Router)
-
+const Recommend = (resolve) => {
+  import('components/recommend/recommend').then((module) => {
+    resolve(module)
+  })
+}
+const Search = (resolve) => {
+  import('components/search/search').then((module) => {
+    resolve(module)
+  })
+}
+const Singer = (resolve) => {
+  import('components/singer/singer').then((module) => {
+    resolve(module)
+  })
+}
+const SingerDetail = (resolve) => {
+  import('components/singer-detail/singer-detail').then((module) => {
+    resolve(module)
+  })
+}
+const NotFind = (resolve) => {
+  import('components/notfind/notfind').then((module) => {
+    resolve(module)
+  })
+}
+const RecommendList = (resolve) => {
+  import('components/recommend-list/recommend-list').then((module) => {
+    resolve(module)
+  })
+}
+const RankDetail = (resolve) => {
+  import('components/rank-detail/rank-detail').then((module) => {
+    resolve(module)
+  })
+}
+const userCenter = (resolve) => {
+  import('components/user-center/user-center').then((module) => {
+    resolve(module)
+  })
+}
+const Rank = (resolve) => {
+  import('components/rank/rank').then((module) => {
+    resolve(module)
+  })
+}
 export default new Router({
   routes: [
     {
